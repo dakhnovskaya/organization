1c0297bdcb18a9bb0a36fc346c34ebc738f7838e
Dakhnovskaya Olga <lokomoivvv@bk.ru>
2018-10-09 20:27:21 +0400

    Добавлена работа в админке

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import District, Company, Category, EnterpriseNetwork, Product, CompanyProduct
 
-# Register your models here.
+
+class CompanyProductInline(admin.TabularInline):
+    model = CompanyProduct
+    extra = 1
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    inlines = (CompanyProductInline,)
+
+
+admin.site.register(District)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Category)
+admin.site.register(EnterpriseNetwork)
+admin.site.register(Product)
--- app/models.py
+++ app/models.py
@@ -9,13 +9,34 @@ class RepresentationFieldNameMixin:
 class District(RepresentationFieldNameMixin, models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
 
+    class Meta:
+        verbose_name = 'Район города'
+        verbose_name_plural = 'Районы города'
+
 
 class Category(RepresentationFieldNameMixin, models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
 
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
 
 class EnterpriseNetwork(RepresentationFieldNameMixin, models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Сеть предприятий'
+        verbose_name_plural = 'Сети предприятий'
+
+
+class Product(RepresentationFieldNameMixin, models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'Услуга\товар'
+        verbose_name_plural = 'Услуги\товары'
 
 
 class Company(RepresentationFieldNameMixin, models.Model):
@@ -23,16 +44,18 @@ class Company(RepresentationFieldNameMixin, models.Model):
     description = models.CharField(max_length=500, verbose_name='Описание')
     enterprise_network = models.ForeignKey(EnterpriseNetwork, verbose_name='Сеть предприятий', on_delete=models.PROTECT)
     district = models.ManyToManyField(District, verbose_name='Район города')
+    products = models.ManyToManyField(Product, through='CompanyProduct', verbose_name='Предприятие')
+
+    class Meta:
+        verbose_name = 'Предприятие'
+        verbose_name_plural = 'Предприятия'
 
 
-class Product(RepresentationFieldNameMixin, models.Model):
-    name = models.CharField(max_length=255, verbose_name='Название')
-    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.PROTECT)
-    company = models.ManyToManyField(Company, through='CompanyProduct', verbose_name='Предприятие')
-
-
-class CompanyProduct(RepresentationFieldNameMixin, models.Model):
-    company = models.ForeignKey(Company, verbose_name='Предприятие')
-    product = models.ForeignKey(Product, verbose_name='Услуга\товар')
+class CompanyProduct(models.Model):
+    company = models.ForeignKey(Company, verbose_name='Предприятие', on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, verbose_name='Услуга\товар', on_delete=models.PROTECT)
     cost = models.FloatField(verbose_name='Цена')
 
+    class Meta:
+        verbose_name = 'Услуга\товар предприятия'
+        verbose_name_plural = 'Услуги\товары предприятия'
